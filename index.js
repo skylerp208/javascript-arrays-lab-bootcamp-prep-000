aed94ee2598e9d5e5d005be27aa45980d4c2ebdb
skylerwphillipstx@gmail.com Phillips <skylerwphillipstx@gmail.com>
2018-07-30 21:42:20 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -22,6 +22,6 @@ function appendKitten(name) {
 function prependKitten(name){
   return [name, ...kittens]
 }
-function removeFirstKitten(){
-  return slice.kittens(2,3)
+function removeFirstKitten() {
+  
 }
